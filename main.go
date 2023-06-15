55c37bb7fbbe0c049c2c2183a16fa8974eb5f82d
gleisoncs <gleisoncs@gmail.com>
2023-06-14 23:04:20 -0300

    go routines

--- main.go
+++ main.go
@@ -1,20 +1,18 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
+	"sync"
 
 	"github.com/gin-gonic/gin"
 )
 
-func main() {
-	r := gin.New()
+const totalWorker = 10
 
-	r.GET("/books", listBooksHandler)
-	r.POST("/books", createBookHandler)
-	r.DELETE("/books/:id", deleteBookHandler)
-
-	r.Run()
-}
+var jobs = make(chan []interface{}, 0)
+var wg = new(sync.WaitGroup)
 
 type Book struct {
 	ID     string `json:"id"`
@@ -26,6 +24,56 @@ var books = []Book{
 	{ID: "1", Title: "Harry Potter", Author: "J. K. Rowling"},
 	{ID: "2", Title: "The Lord of the Rings", Author: "J. R. R. Tolkien"},
 	{ID: "3", Title: "The Wizard of Oz", Author: "L. Frank Baum"},
+}
+
+func main() {
+	r := gin.New()
+
+	go dispatchWorkers(jobs, wg)
+
+	r.GET("/books", listBooksHandler)
+	r.POST("/books", createBookHandler)
+	r.DELETE("/books/:id", deleteBookHandler)
+
+	wg.Wait()
+
+	r.Run()
+}
+
+func dispatchWorkers(jobs <-chan []interface{}, wg *sync.WaitGroup) {
+	for workerIndex := 0; workerIndex <= totalWorker; workerIndex++ {
+		go func(workerIndex int, jobs <-chan []interface{}, wg *sync.WaitGroup) {
+			counter := 0
+
+			for job := range jobs {
+				doTheJob(workerIndex, counter, job)
+				wg.Done()
+				counter++
+			}
+		}(workerIndex, jobs, wg)
+	}
+}
+
+func doTheJob(workerIndex, counter int, values []interface{}) {
+	for {
+		var outerError error
+		func(outerError *error) {
+			defer func() {
+				if err := recover(); err != nil {
+					*outerError = fmt.Errorf("%v", err)
+				}
+			}()
+
+			//log.Println(values)
+		}(&outerError)
+		if outerError == nil {
+			break
+		}
+	}
+
+	if counter%10 == 0 {
+		log.Println("=> worker", workerIndex, "inserted", counter, "data")
+	}
 }
 
 func listBooksHandler(c *gin.Context) {
@@ -42,9 +90,14 @@ func createBookHandler(c *gin.Context) {
 		return
 	}
 
-	books = append(books, book)
+	newTransaction := make([]interface{}, 0)
+	newTransaction = append(newTransaction, book)
 
-	c.JSON(http.StatusCreated, book)
+	wg.Add(1)
+	jobs <- newTransaction
+	//books = append(books, book)
+
+	c.JSON(http.StatusCreated, nil)
 }
 
 func deleteBookHandler(c *gin.Context) {
