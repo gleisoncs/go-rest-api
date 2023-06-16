1d7b95b8e2cce99299cd5e3eb7653bab11c1fddb
gleisoncs <gleisoncs@gmail.com>
2023-06-16 16:56:30 -0300

    dynamod

--- dynamo/main.go
+++ dynamo/main.go
@@ -4,12 +4,18 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 	"sync"
 
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 )
 
-const totalWorker = 5
+const totalWorker = 10
 
 var jobs = make(chan []interface{}, 0)
 var wg = new(sync.WaitGroup)
@@ -20,13 +26,20 @@ type Book struct {
 	Author string `json:"author"`
 }
 
+type Item struct {
+	Year   int
+	Title  string
+	Plot   string
+	Rating float64
+}
+
 var books = []Book{
 	{ID: "1", Title: "Harry Potter", Author: "J. K. Rowling"},
 	{ID: "2", Title: "The Lord of the Rings", Author: "J. R. R. Tolkien"},
 	{ID: "3", Title: "The Wizard of Oz", Author: "L. Frank Baum"},
 }
 
-func main() {
+func main2() {
 	r := gin.New()
 
 	go dispatchWorkers(jobs, wg)
@@ -55,22 +68,47 @@ func dispatchWorkers(jobs <-chan []interface{}, wg *sync.WaitGroup) {
 }
 
 func doTheJob(workerIndex, counter int, values []interface{}) {
-	for {
-		var outerError error
-		func(outerError *error) {
-			defer func() {
-				if err := recover(); err != nil {
-					*outerError = fmt.Errorf("%v", err)
-				}
-			}()
 
-			log.Println(values)
-		}(&outerError)
-		if outerError == nil {
-			break
-		}
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+
+	// Create DynamoDB client
+	svc := dynamodb.New(sess)
+	// snippet-end:[dynamodb.go.create_item.session]
+
+	id := uuid.New()
+
+	// snippet-start:[dynamodb.go.create_item.assign_struct]
+	item := Item{
+		Title:  id.String(),
+		Year:   2015,
+		Plot:   "Nothing happens at all.",
+		Rating: 0.0,
 	}
 
+	av, err := dynamodbattribute.MarshalMap(item)
+	if err != nil {
+		log.Fatalf("Got error marshalling new movie item: %s", err)
+	}
+	// snippet-end:[dynamodb.go.create_item.assign_struct]
+
+	// snippet-start:[dynamodb.go.create_item.call]
+	// Create item in table Movies
+	tableName := "Movies"
+
+	input := &dynamodb.PutItemInput{
+		Item:      av,
+		TableName: aws.String(tableName),
+	}
+
+	_, err = svc.PutItem(input)
+	if err != nil {
+		log.Println("Got error calling PutItem: " + err.Error())
+	} else {
+		year := strconv.Itoa(item.Year)
+		fmt.Println("Successfully added '" + item.Title + "' (" + year + ") to table " + tableName)
+	}
 	if counter%10 == 0 {
 		log.Println("=> worker", workerIndex, "inserted", counter, "data")
 	}
@@ -95,7 +133,7 @@ func createBookHandler(c *gin.Context) {
 
 	wg.Add(1)
 	jobs <- newTransaction
-	books = append(books, book)
+	//books = append(books, book)
 
 	c.JSON(http.StatusCreated, nil)
 }
